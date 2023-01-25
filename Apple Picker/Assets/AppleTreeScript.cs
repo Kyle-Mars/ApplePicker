daab34ddf993f2825a5266e893567193ba616880
KylelMars <kylelmars@gmail.com>
2023-01-25 17:00:04 -0600

    Began Apple Picker Project

---
+++ Apple Picker/Assets/AppleTreeScript.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class AppleTreeScript : MonoBehaviour
+{
+    public GameObject Apple;
+
+    public float appleDropTime = 1.0f;
+    public float speed = 3.0f;
+    public float percentChangeDirection = 0.1f;
+    public float edgeDistance = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DropApple", appleDropTime);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(Apple);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", appleDropTime);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if(pos.x < -edgeDistance && speed < 0 || pos.x > edgeDistance && speed > 0)
+        {
+            speed *= -1f;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 pos = transform.position;
+        if (-edgeDistance <= pos.x && edgeDistance >= pos.x && UnityEngine.Random.value < percentChangeDirection)
+        {
+            speed *= -1;
+        }
+    }
+}
